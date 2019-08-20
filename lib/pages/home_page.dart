b6c1d503d0cdd39f27c5364d080d948b27e9276c
zengjunxiang <zengjunxiang@eetown.com>
2019-08-20 16:29:03 +0800

    完成首页轮播功能

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import '../config/httpHeaders.dart';
 import '../config/service_url.dart';
 import '../service/service_method.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget{
 
@@ -13,35 +15,79 @@ class HomePage extends StatefulWidget{
 class _HomePageState extends State<HomePage> {
 
 
-  String homePageContent = '还没有请求数据';
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    getHomePageContent().then((val) {
-      setState(() {
-        homePageContent = val.toString();
-      });
-    });
-
-
-    super.initState();
-  }
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('百姓生活+'),
       ),
 
-      body: SingleChildScrollView(
-        child: Text(homePageContent),
+      body: FutureBuilder(
+
+        future: getHomePageContent(),
+        builder: (context,snapshot){
+
+          if(snapshot.hasData){
+
+            var data=json.decode(snapshot.data.toString());
+            List<Map> swiperDataList = (data['data']['slides'] as List).cast(); // 顶部轮播组件数
+
+
+            return Column(
+              children: <Widget>[
+                SwiperDiy(swiperDataList:swiperDataList),
+              ],
+            );
+
+
+          }else{
+
+            return Center (
+
+              child: Text('加载中'),
+            );
+          }
+
+        },
       ),
 
     );
   }
 
 }
+
+
+class SwiperDiy extends StatelessWidget{
+
+  final List swiperDataList;
+
+  SwiperDiy({Key key ,this.swiperDataList}) :super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+       height: 200.0,
+      child: Swiper(
+
+          itemBuilder: ( BuildContext context,int index){
+            return Image.network("${swiperDataList[index]['image']}",fit: BoxFit.fill);
+          },
+
+          itemCount: swiperDataList.length,
+          pagination: new SwiperPagination(),
+          autoplay: true,
+
+
+      ),
+
+    );
+  }
+
+
+
+
+}
+
 
 
 
