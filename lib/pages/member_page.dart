926eb591b6c80a3bd0c422050130c7ab148b36b4
zengjunxiang <zengjunxiang@eetown.com>
2019-08-21 18:44:22 +0800

    我的订单区域 UI制作与实现

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -18,6 +18,7 @@ class MemberPage extends StatelessWidget {
 
           _topHeader(),
           _orderTitle(),
+          _orderType()
 
         ],
       ),
@@ -70,6 +71,74 @@ class MemberPage extends StatelessWidget {
         leading: Icon(Icons.list),
         title:Text('我的订单'),
         trailing: Icon(Icons.arrow_right),
+      ),
+    );
+
+  }
+
+
+  //订单区域
+  Widget _orderType(){
+
+    return Container(
+      margin: EdgeInsets.only(top:5),
+      width: ScreenUtil().setWidth(750),
+      height: ScreenUtil().setHeight(150),
+      padding: EdgeInsets.only(top:20),
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.party_mode,
+                  size: 30,
+                ),
+                Text('待付款'),
+              ],
+            ),
+          ),
+          //-----------------
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.query_builder,
+                  size: 30,
+                ),
+                Text('待发货'),
+              ],
+            ),
+          ),
+          //-----------------
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.directions_car,
+                  size: 30,
+                ),
+                Text('待收货'),
+              ],
+            ),
+          ),
+          Container(
+            width: ScreenUtil().setWidth(187),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.content_paste,
+                  size: 30,
+                ),
+                Text('待评价'),
+              ],
+            ),
+          ),
+        ],
       ),
     );
 
