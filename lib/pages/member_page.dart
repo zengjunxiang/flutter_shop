fdb8516830539a71206adf53dd1f9229a23ecd55
zengjunxiang <zengjunxiang@eetown.com>
2019-08-21 18:16:30 +0800

    完成会员中心顶部头像区域的UI制作

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MemberPage extends StatelessWidget {
 
@@ -7,10 +8,49 @@ class MemberPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Center(
-        child: Text('会员中心页'),
+
+      appBar: new AppBar(
+        title: new Text('会员中心'),
+      ),
+
+      body: ListView(
+        children: <Widget>[
+
+          _topHeader(),
+
+        ],
+      ),
+
+    );
+  }
+
+
+  Widget _topHeader(){
+
+    return Container(
+      child: Column(
+        children: <Widget>[
+
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: ClipOval(
+              child: Image.network('http://blogimages.jspang.com/blogtouxiang1.jpg'),
+            ),
+
+      ),
+
+       Container(
+         margin: EdgeInsets.only(top: 20),
+         child: new Text('胖兄',
+         style: new TextStyle(
+           fontSize: ScreenUtil().setSp(48),
+           color: Colors.grey,
+         ),),
+       )
+        ],
       ),
     );
   }
+
 
 }
