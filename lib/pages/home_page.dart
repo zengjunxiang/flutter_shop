703187160afb296e235c49d7f3cf129df94b2b26
zengjunxiang <zengjunxiang@eetown.com>
2019-08-21 09:34:21 +0800

    '解决首页展示页越界bug,添加滑动'

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,6 +6,7 @@ import '../config/service_url.dart';
 import '../service/service_method.dart';
 import 'dart:convert';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget{
     _HomePageState createState() => _HomePageState();
@@ -35,18 +36,22 @@ class _HomePageState extends State<HomePage> {
             String  advertesPicture = data['data']['advertesPicture']['PICTURE_ADDRESS']; //广告图片
             String  leaderImage= data['data']['shopInfo']['leaderImage'];  //店长图片
             String  leaderPhone = data['data']['shopInfo']['leaderPhone']; //店长电话
+            List<Map> recommendList = (data['data']['recommend'] as List).cast(); // 商品推荐
 
             if(navigatorList.length>10){
                 navigatorList.removeRange(10, navigatorList.length);
             }
 
-            return Column(
-              children: <Widget>[
-                SwiperDiy(swiperDataList:swiperDataList), //轮播组件
-                TopNavigator(navigatorList:navigatorList),  //导航组件
-                AdBanner(advertesPicture:advertesPicture),  //广告组件
-                LeaderPhone(leaderImage:leaderImage,leaderPhone:leaderPhone) 
-              ],
+            return SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  SwiperDiy(swiperDataList:swiperDataList), //轮播组件
+                  TopNavigator(navigatorList:navigatorList),  //导航组件
+                  AdBanner(advertesPicture:advertesPicture),  //广告组件
+                  LeaderPhone(leaderImage:leaderImage,leaderPhone:leaderPhone),
+                  Recommend(recommendList:recommendList),
+                ],
+              ),
             );
 
 
@@ -166,28 +171,123 @@ class AdBanner extends StatelessWidget{
 
 }
 
+
+//拨打电话组件的实现
 class LeaderPhone extends StatelessWidget{
 
   final String leaderImage; //店长图片
   final String leaderPhone; //店长电话
 
   LeaderPhone({Key key,this.leaderImage, this.leaderPhone}):super(key:key);
-
-
-
-
+  
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
       child: InkWell(
-        onTap: (){},
+        onTap: _launchURL,
         child: Image.network(leaderImage),
       ),
     );
   }
 
 
+  void _launchURL() async{
+
+    String url = 'tel:'+leaderPhone;
+
+    if(await canLaunch(url)){
+      await launch(url);
+    }else{
+      throw 'Could not launch $url';
+    }
+  }
+
+
+}
+
+
+//商品推荐
+class Recommend extends StatelessWidget {
+  final List  recommendList;
+
+  Recommend({Key key, this.recommendList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(380),
+      margin: EdgeInsets.only(top: 10.0),
+      child: Column(
+        children: <Widget>[
+          _titleWidget(),
+          _recommedList()
+        ],
+      ),
+    );
+  }
+
+//推荐商品标题
+  Widget _titleWidget(){
+    return Container(
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.fromLTRB(10.0, 2.0, 0,5.0),
+        decoration: BoxDecoration(
+            color:Colors.white,
+            border: Border(
+                bottom: BorderSide(width:0.5,color:Colors.black12)
+            )
+        ),
+        child:Text(
+            '商品推荐',
+            style:TextStyle(color:Colors.pink)
+        )
+    );
+  }
+
+  Widget _recommedList(){
+
+    return Container(
+      height: ScreenUtil().setHeight(330),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: recommendList.length,
+        itemBuilder: (context,index){
+          return _item(index);
+        },
+      ),
+    );
+  }
+
+  Widget _item(index){
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        height: ScreenUtil().setHeight(330),
+        width: ScreenUtil().setWidth(250),
+        padding: EdgeInsets.all(8.0),
+        decoration:BoxDecoration(
+            color:Colors.white,
+            border:Border(
+                left: BorderSide(width:0.5,color:Colors.black12)
+            )
+        ),
+        child: Column(
+          children: <Widget>[
+            Image.network(recommendList[index]['image']),
+            Text('￥${recommendList[index]['mallPrice']}'),
+            Text(
+              '￥${recommendList[index]['price']}',
+              style: TextStyle(
+                  decoration: TextDecoration.lineThrough,
+                  color:Colors.grey
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 
