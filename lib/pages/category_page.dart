eeec643a6e48b53a3bad4179235ed8be800322e9
zengjunxiang <zengjunxiang@eetown.com>
2019-08-22 10:45:18 +0800

    调通 分类 类别接口

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/config/service_url.dart';
+import 'dart:convert';
+import '../service/service_method.dart';
 
-class CategoryPage extends StatelessWidget {
+class CategoryPage extends StatefulWidget {
 
+  _CategoryPageState createState() => _CategoryPageState();
+
+}
+
+
+class _CategoryPageState extends State {
 
   @override
   Widget build(BuildContext context) {
+
+    _getCategory();
+
     // TODO: implement build
-    return Scaffold(
-      body: Center(
-        child: Text('分类页'),
+    return Container(
+      child:Center(
+          child: Text('fen'),
       ),
     );
   }
+
+
+  void _getCategory() async {
+
+    await request('getCategory').then((val) {
+                 var data = json.decode(val.toString());
+                    print(data);
+    });
+  }
+
 
 }
